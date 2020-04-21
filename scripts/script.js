9fdd09f2d77e83703021896386dd67bba0e1523d
vmolash <molash.v@gmail.com>
2020-04-20 23:04:22 -0700

    added background images

---
+++ scripts/script.js
@@ -0,0 +1,23 @@
+$('.menu').on('click', function() {
+    $(this).toggleClass('active');
+    $('.overlay').toggleClass('menu-open');
+  });
+  
+  $('.nav a').on('click', function() {
+    $('.menu').removeClass('active');
+    $('.overlay').removeClass('menu-open');
+  });
+  // scroll top button
+$(document).ready(function(){
+    $(window).scroll(function(){
+      if ($(this).scrollTop() > 40) {
+        $('#topBtn').fadeIn();
+      } else {
+        $('#topBtn').fadeOut();
+      }
+    });
+  
+    $('#topBtn').click(function(){
+      $('html, body').animate({ scrollTop: 0}, 800);
+    });
+  });
