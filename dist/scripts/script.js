dd28d501f3e91b492de7e537f3263635eb37d7c0
vmolash <molash.v@gmail.com>
2020-04-22 23:19:18 -0700

    added js, animation

--- dist/scripts/script.js
+++ dist/scripts/script.js
@@ -1,9 +1,9 @@
 // active link sets to green color when click on it
-let navContainer = document.getElementById('nav');
-let navItems = navContainer.getElementsByClassName('nav-item');
+const NAVCONTAINER = document.querySelector('nav');
+const NAVITEMS = NAVCONTAINER.getElementsByClassName('nav-item');
 
-for(let i = 0; i < navItems.length; i++) {
-  navItems[i].addEventListener('click', function() {
+for(let i = 0; i < NAVITEMS.length; i++) {
+  NAVITEMS[i].addEventListener('click', function() {
     let current = document.getElementsByClassName('active');
     current[0].className = current[0].className.replace(' active', '');
     this.className += ' active';
@@ -11,33 +11,55 @@ for(let i = 0; i < navItems.length; i++) {
 }
 
 // paragraph on the Coctact section changes text color to green with delay
-let clickedContactLink = document.querySelector('[href="#contact"]');
-console.log(clickedContactLink)
-let parConctact = document.getElementsByClassName('connect');
-console.log(parConctact[0]);
+const CLICKEDCONTACTLINK = document.querySelector('[href="#contact"]');
 
-clickedContactLink.addEventListener('click', function() {
+let PARCONTACT = document.getElementsByClassName('connect');
+
+
+CLICKEDCONTACTLINK.addEventListener('click', function() {
   setTimeout(changeColor, 2000);
 })
 function changeColor() {
-  parConctact[0].classList.add('active');
+  PARCONTACT[0].classList.add('active');
 }
 
 // logo VM changes to Volha Molash when hover over on it
-let logoName = document.querySelector('.logo');
-console.log(logoName);
-logoName.addEventListener('mouseover', function() {
+const LOGONAME = document.querySelector('.logo');
+
+LOGONAME.addEventListener('mouseover', function() {
   this.innerHTML = 'Volha Molash';
-  let stringName = this.textContent.split(' ');
+  let stringName = this.textContent.split(' '); // convert to array
   console.log(stringName);
   let hoverName = '';
   for(let i = 0; i < stringName.length; i++) {
     hoverName += stringName[i].charAt(0) + stringName[i].slice(1).toLowerCase() + ' ';
   }
-  console.log(hoverName);
   this.innerHTML = hoverName;
   this.style.transition = "all 1s ease-out";
 });
-logoName.addEventListener('mouseout', function(e) {
+LOGONAME.addEventListener('mouseout', function(e) {
   e.target.innerHTML = 'VM';
 });
+
+// Hide navigation on mobile version when click on any link
+console.log(window.screen.width);
+const NAVIGAT = document.querySelector('nav');
+const LAB = document.querySelector('label');
+
+if(window.screen.width < 500) {
+  LAB.addEventListener('click', function() {
+    NAVIGAT.classList.toggle('show');
+    
+  });
+  NAVIGAT.addEventListener('click', function() {
+    this.classList.toggle('show');
+    
+  });
+}
+
+const HOME = document.querySelector('.home');
+
+if(window.screen.width > 630) {
+  HOME.parentNode.removeChild(HOME);
+}
+
