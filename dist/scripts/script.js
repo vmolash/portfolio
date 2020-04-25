1f928caf3c21bc3b5a9eb42fa60b0c20bdaf1cc9
vmolash <molash.v@gmail.com>
2020-04-24 17:42:56 -0700

    added js to Projects cards

--- dist/scripts/script.js
+++ dist/scripts/script.js
@@ -63,3 +63,47 @@ if(window.screen.width > 630) {
   HOME.parentNode.removeChild(HOME);
 }
 
+// projects Show Description div on click
+// const PROJECT = document.querySelector('.face');
+// console.log(PROJECT);
+// const DESCRIPTION = document.querySelector('.description');
+// console.log(DESCRIPTION);
+// PROJECT.addEventListener('click', function(e) {
+//   e.preventDefault();
+//   if( (e.target.parentNode.parentNode.classList).contains('face')) {
+//     console.log('Congrants');
+//     console.log(e.target.parentNode.parentNode);
+//     DESCRIPTION.classList.toggle('showDescription');
+//   }
+// });
+// DESCRIPTION.addEventListener('click', function(e) {
+//   this.classList.toggle('showDescription');
+// });
+
+// PROJECT card animation 
+const CARDCONTENT = document.getElementsByClassName('card-content'); // all div.card-content
+const CARDREVEAL = document.querySelectorAll('.card-reveal h4'); // all h4 of div.card-reveal
+console.log(CARDREVEAL);
+for(let i = 0; i < CARDCONTENT.length; i++) {
+  CARDCONTENT[i].addEventListener('click', revealCard);
+}
+// reveal/show card when content card is clicked
+function revealCard(e) {
+  e.preventDefault();
+
+  let myID = this.id; // store clicked div.card-content ID
+  let revealID = myID.replace('content', 'reveal'); // store div.card-reveal of clicked div.card-content
+  document.getElementById(revealID).classList.toggle('activeCard'); //
+};
+
+// hide reveal card when reveal card is clicked
+for(let i = 0; i < CARDREVEAL.length; i++) {
+  CARDREVEAL[i].addEventListener('click', hideRevealCard);
+}
+function hideRevealCard(e) {
+    this.parentNode.classList.remove('activeCard');
+}
+
+
+
+
